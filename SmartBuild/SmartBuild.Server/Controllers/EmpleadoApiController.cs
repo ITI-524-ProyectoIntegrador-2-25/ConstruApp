c731f27a2901297c38ebe871c0517bb01733028c
Frankms123 <frmorasa@est.utn.ac.cr>
2025-07-11 14:53:06 -0600

    Modulos Actualizados

--- SmartBuild/SmartBuild.Server/Controllers/EmpleadoApiController.cs
+++ SmartBuild/SmartBuild.Server/Controllers/EmpleadoApiController.cs
@@ -2,103 +2,103 @@
 using Microsoft.AspNetCore.Mvc;
 using Models.CRM;
 using Models.GNR;
+using Models.GPR;
 
 namespace SmartBuild.Server.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class ContactApiController : Controller
+    public class EmpleadoApiController : Controller
     {
         private readonly ILogger<ContactApiController> _logger;
         private readonly ICRMConnectionDB _repository;
 
-        public ContactApiController(ILogger<ContactApiController> logger, ICRMConnectionDB repository)
+        public EmpleadoApiController(ILogger<ContactApiController> logger, ICRMConnectionDB repository)
         {
             _logger = logger;
             _repository = repository;
         }
 
-        #region Contactos
-        [Route("GetContacts")]
+        #region Empleado
+        [Route("GetEmpleado")]
         [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Contacto))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Empleado))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public List<Contacto> GetContacts(string usuario)
+        public List<Empleado> GetEmpleado(string usuario)
         {
             try
             {
-                var contactData = _repository.GetContacts(usuario);
-                return contactData;
+                var EmpleadoData = _repository.GetEmpleados(usuario);
+                return EmpleadoData;
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, nameof(GetContacts));
+                _logger.LogError(ex, nameof(GetEmpleado));
                 throw;
             }
         }
 
-        [Route("GetContactInfo")]
+        [Route("GetEmpleadoInfo")]
         [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Contacto))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Empleado))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public List<Contacto> GetContactInfo(int idContacto, string usuario)
+        public List<Empleado> GetEmpleadobyInfo(int idEmpleado, string usuario)
         {
             try
             {
-                var contactData = _repository.GetContactInfo(idContacto, usuario);
+                var contactData = _repository.GetEmpleadobyInfo(idEmpleado, usuario);
                 return contactData;
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, nameof(GetContactInfo));
+                _logger.LogError(ex, nameof(GetEmpleadobyInfo));
                 throw;
             }
         }
 
-        [Route("InsertContact")]
+        [Route("InsertEmpleado")]
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Contacto))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Empleado))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public List<Response> InsertContact(Contacto contacto)
+        public List<Response> InsertEmpleado(Empleado empleado)
         {
             try
             {
-                var res = _repository.InsertContact(contacto);
+                var res = _repository.InsertEmpleado(empleado);
                 return res;
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, nameof(InsertContact));
+                _logger.LogError(ex, nameof(InsertEmpleado));
                 throw;
             }
         }
 
-        [Route("UpdateContact")]
+        [Route("UpdateEmpleado")]
         [HttpPut]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Contacto))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public List<Response> UpdateContact(Contacto contacto)
+        public List<Response> UpdateEmpleado(Empleado empleado)
         {
             try
             {
-                var res = _repository.UpdateContact(contacto);
+                var res = _repository.UpdateEmpleado(empleado);
                 return res;
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, nameof(UpdateContact));
+                _logger.LogError(ex, nameof(UpdateEmpleado));
                 throw;
             }
         }
         #endregion
     }
 }
-        
--- SmartBuild/SmartBuild.Server/Controllers/UsuarioApiController.cs
+++ SmartBuild/SmartBuild.Server/Controllers/UsuarioApiController.cs
@@ -2,103 +2,103 @@
 using Microsoft.AspNetCore.Mvc;
 using Models.CRM;
 using Models.GNR;
+using Models.GPR;
 
 namespace SmartBuild.Server.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class ContactApiController : Controller
+    public class UsuarioApiController : Controller
     {
-        private readonly ILogger<ContactApiController> _logger;
+        private readonly ILogger<EmpleadoApiController> _logger;
         private readonly ICRMConnectionDB _repository;
 
-        public ContactApiController(ILogger<ContactApiController> logger, ICRMConnectionDB repository)
+        public UsuarioApiController(ILogger<EmpleadoApiController> logger, ICRMConnectionDB repository)
         {
             _logger = logger;
             _repository = repository;
         }
 
-        #region Contactos
-        [Route("GetContacts")]
+        #region Usuario
+        [Route("GetUsuario")]
         [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Contacto))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Usuario))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public List<Contacto> GetContacts(string usuario)
+        public List<Usuario> GetUsuario(string usuario)
         {
             try
             {
-                var contactData = _repository.GetContacts(usuario);
-                return contactData;
+                var userData = _repository.GetUsuario(usuario);
+                return userData;
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, nameof(GetContacts));
+                _logger.LogError(ex, nameof(GetUsuario));
                 throw;
             }
         }
 
-        [Route("GetContactInfo")]
+        [Route("GetUsuarioInfo")]
         [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Contacto))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Usuario))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public List<Contacto> GetContactInfo(int idContacto, string usuario)
+        public List<Usuario> GetUsuarioInfo(int idUsuario, string usuario)
         {
             try
             {
-                var contactData = _repository.GetContactInfo(idContacto, usuario);
-                return contactData;
+                var userData = _repository.GetUsuariobyInfo(idUsuario, usuario);
+                return userData;
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, nameof(GetContactInfo));
+                _logger.LogError(ex, nameof(GetUsuarioInfo));
                 throw;
             }
         }
 
-        [Route("InsertContact")]
+        [Route("InsertUsuario")]
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Contacto))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Usuario))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public List<Response> InsertContact(Contacto contacto)
+        public List<Response> InsertUsuario(Usuario usuario)
         {
             try
             {
-                var res = _repository.InsertContact(contacto);
+                var res = _repository.InsertUsuario(usuario);
                 return res;
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, nameof(InsertContact));
+                _logger.LogError(ex, nameof(InsertUsuario));
                 throw;
             }
         }
 
-        [Route("UpdateContact")]
+        [Route("UpdateUsuario")]
         [HttpPut]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Contacto))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Usuario))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public List<Response> UpdateContact(Contacto contacto)
+        public List<Response> UpdateUsuario(Usuario usuario)
         {
             try
             {
-                var res = _repository.UpdateContact(contacto);
+                var res = _repository.UpdateUsuario(usuario);
                 return res;
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, nameof(UpdateContact));
+                _logger.LogError(ex, nameof(UpdateUsuario));
                 throw;
             }
         }
         #endregion
     }
 }
-        
